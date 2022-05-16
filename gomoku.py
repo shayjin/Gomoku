d870fbdf247b817cea5e7465711c666ff7a3bb5b
Jay Shin <83435667+shayjin@users.noreply.github.com>
2022-05-16 14:23:53 -0400

    added dumb computer

--- gomoku.py
+++ gomoku.py
@@ -1,5 +1,4 @@
-
-import pygame, sys
+import pygame, sys, random, time
 
 LENGTH = 760
 
@@ -17,26 +16,27 @@ screen = pygame.display.set_mode((LENGTH, LENGTH))
 pygame.display.set_caption("Gomoku")
 screen.fill(board_yor)
 
-
 board = []
 turn = 'Black'
 
 def create_board():
     global board
+    board = []
     for i in range(19):
         board.append([0]* 19)
-    
-    pygame.draw.line(screen, line_yor, (offset, offset), (offset, LENGTH-offset), line_width) 
-    pygame.draw.line(screen, line_yor, (offset, offset), (LENGTH-offset,offset), line_width) 
-    pygame.draw.line(screen, line_yor, (LENGTH-offset,LENGTH-offset), (LENGTH-offset,offset), line_width) 
-    pygame.draw.line(screen, line_yor, (LENGTH-offset,LENGTH-offset), (offset, LENGTH-offset), line_width)
-
+        
     line = 0
     
+    screen.fill(board_yor)
     while line < 19:
         pygame.draw.line(screen, line_yor, (line*offset*2+offset,offset), (line*offset*2+offset,LENGTH-offset), line_width) 
         pygame.draw.line(screen, line_yor, (offset, line*offset*2+offset), (LENGTH-offset, line*offset*2+offset), line_width) 
         line += 1
+        
+    pygame.draw.line(screen, line_yor, (offset, offset), (offset, LENGTH-offset), line_width) 
+    pygame.draw.line(screen, line_yor, (offset, offset), (LENGTH-offset,offset), line_width) 
+    pygame.draw.line(screen, line_yor, (LENGTH-offset,LENGTH-offset), (LENGTH-offset,offset), line_width) 
+    pygame.draw.line(screen, line_yor, (LENGTH-offset,LENGTH-offset), (offset, LENGTH-offset), line_width)
     
 def switch_turn():
     global turn
@@ -56,53 +56,53 @@ def move(x, y):
             switch_turn()
             
 def check_vertical(x, y):
-    if board[x][y] == board[x][y-1] == board[x][y-2] == board[x][y-3] == board[x][y-4]: 
+    if y >= 4 and board[x][y] == board[x][y-1] == board[x][y-2] == board[x][y-3] == board[x][y-4]: 
         return True
-    elif board[x][y+1] == board[x][y] == board[x][y-1] == board[x][y-2] == board[x][y-3]: 
+    elif y >= 3 and y < 18 and board[x][y+1] == board[x][y] == board[x][y-1] == board[x][y-2] == board[x][y-3]: 
         return True
-    elif board[x][y+2] == board[x][y+1] == board[x][y] == board[x][y-1] == board[x][y-2]: 
+    elif y >= 2 and y < 17 and board[x][y+2] == board[x][y+1] == board[x][y] == board[x][y-1] == board[x][y-2]: 
         return True
-    elif board[x][y+3] == board[x][y+2] == board[x][y+1] == board[x][y] == board[x][y-1]: 
+    elif y >= 1 and y < 16 and board[x][y+3] == board[x][y+2] == board[x][y+1] == board[x][y] == board[x][y-1]: 
         return True
-    elif board[x][y+4] == board[x][y+3] == board[x][y+2] == board[x][y+1] == board[x][y]: 
+    elif y < 15 and board[x][y+4] == board[x][y+3] == board[x][y+2] == board[x][y+1] == board[x][y]: 
         return True
     else:
         return False
 
 def check_horizontal(x, y):
-    if board[x][y] == board[x-1][y] == board[x-2][y] == board[x-3][y] == board[x-4][y]:
+    if x >= 4 and board[x][y] == board[x-1][y] == board[x-2][y] == board[x-3][y] == board[x-4][y]:
         return True
-    elif board[x+1][y] == board[x][y] == board[x-1][y] == board[x-2][y] == board[x-3][y]:
+    elif x >= 3 and x < 18 and board[x+1][y] == board[x][y] == board[x-1][y] == board[x-2][y] == board[x-3][y]:
         return True
-    elif board[x+2][y] == board[x+1][y] == board[x][y] == board[x-1][y] == board[x-2][y]:
+    elif x >= 2 and x < 17 and board[x+2][y] == board[x+1][y] == board[x][y] == board[x-1][y] == board[x-2][y]:
         return True
-    elif board[x+3][y] == board[x+2][y] == board[x+1][y] == board[x][y] == board[x-1][y]:
+    elif x >= 1 and x < 16 and board[x+3][y] == board[x+2][y] == board[x+1][y] == board[x][y] == board[x-1][y]:
         return True
-    elif board[x+4][y] == board[x+3][y] == board[x+2][y] == board[x+1][y] == board[x][y]:
+    elif x < 15 and board[x+4][y] == board[x+3][y] == board[x+2][y] == board[x+1][y] == board[x][y]:
         return True
     else:
         return False
 
 def check_diagonal(x,y):
-    if board[x][y] == board[x-1][y+1] == board[x-2][y+2] == board[x-3][y+3] == board[x-4][y+4]:
+    if x >= 4 and y < 15 and board[x][y] == board[x-1][y+1] == board[x-2][y+2] == board[x-3][y+3] == board[x-4][y+4]:
         return True
-    elif board[x+1][y-1] == board[x][y] == board[x-1][y+1] == board[x-2][y+2] == board[x-3][y+3]:
+    elif x >= 3 and x < 18 and y >= 1 and y < 16 and board[x+1][y-1] == board[x][y] == board[x-1][y+1] == board[x-2][y+2] == board[x-3][y+3]:
         return True
-    elif board[x+2][y-2] == board[x+1][y-1] == board[x][y] == board[x-1][y+1] == board[x-2][y+2]:
+    elif x >= 2 and x < 17 and y >= 2 and y < 17 and board[x+2][y-2] == board[x+1][y-1] == board[x][y] == board[x-1][y+1] == board[x-2][y+2]:
         return True
-    elif board[x+3][y-3] == board[x+2][y-2] == board[x+1][y-1] == board[x][y] == board[x-1][y+1]:
+    elif x >= 1 and x < 16 and y >= 3 and y < 18 and board[x+3][y-3] == board[x+2][y-2] == board[x+1][y-1] == board[x][y] == board[x-1][y+1]:
         return True
-    elif board[x+4][y-4] == board[x+3][y-3] == board[x+2][y-2] == board[x+1][y-1] == board[x][y]:
+    elif x < 15 and y >= 4 and board[x+4][y-4] == board[x+3][y-3] == board[x+2][y-2] == board[x+1][y-1] == board[x][y]:
         return True
-    elif board[x][y] == board[x+1][y+1] == board[x+2][y+2] == board[x+3][y+3] == board[x+4][y+4]:
+    elif x < 15 and y < 15 and board[x][y] == board[x+1][y+1] == board[x+2][y+2] == board[x+3][y+3] == board[x+4][y+4]:
         return True
-    elif board[x-1][y-1] == board[x][y] == board[x+1][y+1] == board[x+2][y+2] == board[x+3][y+3]:
+    elif x >= 1 and x < 16 and y >= 1 and y < 16 and board[x-1][y-1] == board[x][y] == board[x+1][y+1] == board[x+2][y+2] == board[x+3][y+3]:
         return True
-    elif board[x-2][y-2] == board[x-1][y-1] == board[x][y] == board[x+1][y+1] == board[x+2][y+2]:
+    elif x >= 2 and x < 17 and y >= 2 and y < 17 and board[x-2][y-2] == board[x-1][y-1] == board[x][y] == board[x+1][y+1] == board[x+2][y+2]:
         return True
-    elif board[x-3][y-3] == board[x-2][y-2] == board[x-1][y-1] == board[x][y] == board[x+1][y+1]:
+    elif x >= 3 and x < 18 and y >= 3 and y < 18 and board[x-3][y-3] == board[x-2][y-2] == board[x-1][y-1] == board[x][y] == board[x+1][y+1]:
         return True
-    elif board[x-4][y-4] == board[x-3][y-3] == board[x-2][y-2] == board[x-1][y-1] == board[x][y]:
+    elif x >= 4 and y >= 4 and board[x-4][y-4] == board[x-3][y-3] == board[x-2][y-2] == board[x-1][y-1] == board[x][y]:
         return True
     else:
         return False
@@ -114,39 +114,96 @@ def check_win(x, y):
     else: return False
     
 def display_win_message(turn):
-    message = turn + ' wins! Click anywhere to restart...'
+    message = turn + ' wins!!!!!!'
     font_size = int(LENGTH/(len(message))*1.75) # size responsive lol
     
     font = pygame.font.Font('freesansbold.ttf', font_size)
     text = font.render(message, True, (128,0,0), (0,0,128))
     screen.blit(text, (LENGTH / 2 - LENGTH / 2.5 , LENGTH / 2))
     
-def restart():
-    screen.fill(board_yor)
-    create_board()
-    turn = 'Black'
 
+# menu
+def display_main_menu():
+    screen.fill(board_yor)
+    
+    gomoku_font = pygame.font.Font('freesansbold.ttf', int(LENGTH/(len('Gomoku'))))
+    gomoku = gomoku_font.render('Gomoku', True, (128,0,0), (0,0,128))
+    screen.blit(gomoku, (LENGTH / 2 - LENGTH / 3, LENGTH / 7))
+    
+    single_player_font = pygame.font.Font('freesansbold.ttf', int(LENGTH/(len('Singleplayer Mode'))))
+    single_player_mode = single_player_font.render('Singleplayer Mode', True, (128,0,0), (0,0,128))
+    screen.blit(single_player_mode, (LENGTH / 2 - LENGTH / 3.7 , LENGTH / 2))
+    
+    multi_player_font = pygame.font.Font('freesansbold.ttf', int(LENGTH/(len('Multiplayer Mode'))))
+    multi_player_mode = multi_player_font.render('Multiplayer Mode', True, (128,0,0), (0,0,128))
+    screen.blit(multi_player_mode, (LENGTH / 2 - LENGTH / 3.7 , LENGTH / 1.5))
+    
+
+    
+    
+# main method
+in_main_menu = True
+multiplayer = False
 needs_to_restart = False
-create_board()
+display_main_menu()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
-            if not needs_to_restart:
-                x = int(((event.pos[0]) / 40))
-                y = int(((event.pos[1]) / 40))
-                print(x, y)
-                move(x, y)
-                if (check_win(x, y)):
-                    switch_turn()
-                    display_win_message(turn)
-                    needs_to_restart = True
+            if in_main_menu:
+                x = event.pos[0]
+                y = event.pos[1]
+                if x >= 175 and x < 580 and y > 385 and y < 430:
+                    in_main_menu = False
+                    create_board()
+                elif x >= 175 and x < 575 and y > 510 and y < 555:
+                    in_main_menu = False
+                    multiplayer = True
+                    create_board()
             else:
-                needs_to_restart = False
-                restart()
-            
-            
-            
+                if not multiplayer:
+                    if not needs_to_restart:
+                        x = int(((event.pos[0]) / 40))
+                        y = int(((event.pos[1]) / 40))
+                        print(x, y)
+                        move(x, y)
+                        if (check_win(x, y)):
+                            switch_turn()
+                            display_win_message(turn)
+                            needs_to_restart = True
+                    else:
+                        needs_to_restart = False
+                        turn = 'Black'
+                        display_main_menu()
+                else:
+                    if not needs_to_restart:
+                        x = int(((event.pos[0]) / 40))
+                        y = int(((event.pos[1]) / 40))
+                        
+                        
+                        if board[x][y] == 0:
+                            move(x, y)
+                            pc_x = random.randint(0,18)
+                            pc_y = random.randint(0,18)
+                            while board[pc_x][pc_y] != 0:
+                                pc_x = random.randint(0,18)
+                                pc_y = random.randint(0,18)
+                            
+                            move(pc_x,pc_y)
+                        
+                        if (check_win(x, y)):
+                            display_win_message("Black")
+                            needs_to_restart = True
+                        elif (check_win(pc_x, pc_y)):
+                            display_win_message("White")
+                            needs_to_restart = True
+                    else:
+                        needs_to_restart = False
+                        multiplayer = False
+                        in_main_menu = True
+                        turn = 'Black'
+                        display_main_menu()
+                    
     pygame.display.update()
